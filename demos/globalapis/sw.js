872da9896eed4a59351c89944a201fdbf91a155a
toxic-johann <353904974@qq.com>
2017-04-23 23:08:30 +0800

    fix globalapis bug

--- demos/globalapis/sw.js
+++ demos/globalapis/sw.js
@@ -1,6 +1,6 @@
 this.onmessage = function(event) {
   function log () {
-    msg = Array.prototype.slice.call(arguments).join();
+    var msg = Array.prototype.slice.call(arguments).join();
     if (event && event.source) {
       console.log("event.source present");
       event.source.postMessage(msg);
